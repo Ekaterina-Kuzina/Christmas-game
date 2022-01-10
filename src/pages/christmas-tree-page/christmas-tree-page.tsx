5336e98d4d1e1249fd13a696bd1d38d20a97f201
Ekaterina Kuzina <katya.cuzina2011@yandex.ru>
2022-01-10 22:19:11 +0300

    feat: add christmas tree page

--- src/pages/christmas-tree-page/christmas-tree-page.tsx
+++ src/pages/christmas-tree-page/christmas-tree-page.tsx
@@ -1,21 +1,21 @@
-import { url } from "inspector";
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import christmasTreePageStyle from './christmas-tree-page.module.css'
+import data from '../../services/utils/data'
 // import audio from '../../assets/audio/audio.mp3'
 
 const treeData = [
     'https://i.ibb.co/jvMGq3X/1.png',
     'https://i.ibb.co/Cvk3dTW/2.png',
     'https://i.ibb.co/x3JGqCT/3.png',
-    'https://i.ibb.co/LJc4PcJ/4.png', 
-    'https://i.ibb.co/nPYzTDb/5.png', 
+    'https://i.ibb.co/LJc4PcJ/4.png',
+    'https://i.ibb.co/nPYzTDb/5.png',
     'https://i.ibb.co/MD1WGqr/6.png'
 ]
 const bgData = [
     'https://i.ibb.co/WPLvDKW/1.jpg',
     'https://i.ibb.co/YfkTSPs/2.jpg',
     'https://i.ibb.co/8XjMqkQ/3.jpg',
-   ' https://i.ibb.co/N967S8L/4.jpg',
+    'https://i.ibb.co/N967S8L/4.jpg',
     'https://i.ibb.co/4p7VRD8/5.jpg',
     'https://i.ibb.co/R4dqH2F/6.jpg',
     'https://i.ibb.co/RS5PXMx/7.jpg',
@@ -23,28 +23,93 @@ const bgData = [
     'https://i.ibb.co/85dRnHH/9.jpg',
     'https://i.ibb.co/sgZJVhh/10.jpg'
 ]
-export default function ChristmasTreePage () {
-    return(
-        <div className={christmasTreePageStyle.component_wrapper}> 
-            <p>ВЫБЕРИТЕ ЁЛКУ</p>
-            <div className={christmasTreePageStyle.tree_wrapper}>
-                {treeData.map((item)=>{
-                    return(
-                        <div style={{backgroundImage: `url(${item})` }} className={christmasTreePageStyle.tree_block}></div>
-                    )
-                })} 
+export default function ChristmasTreePage() {
+    const [treeState, treeSetState] = useState('https://i.ibb.co/jvMGq3X/1.png')
+    const [bgState, bgSetState] = useState('https://i.ibb.co/WPLvDKW/1.jpg')
+    const treesCollection = useRef<HTMLDivElement>(null);
+    const bgCollection = useRef<HTMLDivElement>(null);
+
+    const addActive = (refItem:any, index: number) => {
+        if (refItem.current) {
+            let arr = Array.from(refItem.current.children);
+            arr.forEach((item:any) => {
+                item.classList.remove(christmasTreePageStyle.active)
+            })
+
+            refItem.current.children[index].classList.add(christmasTreePageStyle.active)
+        }
+    }
+    // const addActive = (index: number) => {
+    //     if (treesCollection.current) {
+    //         let arr = Array.from(treesCollection.current.children);
+
+    //         arr.forEach((item) => {
+    //             item.classList.remove(christmasTreePageStyle.active)
+    //         })
+
+    //         treesCollection.current.children[index].classList.add(christmasTreePageStyle.active)
+    //     }
+    // }
+
+    useEffect(() => {
+        if(treesCollection.current){
+            treesCollection.current.children[0].classList.add(christmasTreePageStyle.active)
+        }
+
+        if(bgCollection.current){
+            bgCollection.current.children[0].classList.add(christmasTreePageStyle.active)
+        }
+        
+    }, [])
+
+    return (
+        <div className={christmasTreePageStyle.wrapper}>
+            <div className={christmasTreePageStyle.components_wrapper}>
+                <p className={christmasTreePageStyle.title}>ВЫБЕРИТЕ ЁЛКУ</p>
+                <div ref={treesCollection} className={christmasTreePageStyle.tree_wrapper}>
+                    {treeData.map((imgItem, index) => {
+                        
+                        return (
+                            <div key={index} style={{ backgroundImage: `url(${imgItem})` }} className={`${christmasTreePageStyle.tree_block}`} onClick={() => {
+                                treeSetState(imgItem)
+                                addActive(treesCollection,index)
+                            }}></div>
+                        )
+                    })}
+                </div>
+
+                <p className={christmasTreePageStyle.title}>ВЫБЕРИТЕ ФОН</p>
+                <div ref={bgCollection} className={christmasTreePageStyle.bg_wrapper}>
+                    {bgData.map((imgItem, index) => {
+                        return (
+                            <div key={index} style={{ backgroundImage: `url(${imgItem})` }} className={christmasTreePageStyle.bg} onClick={() => { 
+                                bgSetState(imgItem) 
+                                addActive(bgCollection,index)
+                            }}></div>
+                        )
+                    })}
+                </div>
             </div>
 
-            <p>ВЫБЕРИТЕ ФОН</p>
-            <div className={christmasTreePageStyle.tree_wrapper}>
-                {bgData.map((item)=>{
-                    return(
-                        <div style={{backgroundImage: `url(${item})` }} className={christmasTreePageStyle.bg}></div>
-                    )
-                })} 
+            <div style={{ backgroundImage: `url(${bgState})` }} className={christmasTreePageStyle.mainTree}>
+                <img src={treeState} alt="tree" />
             </div>
 
+            <div className={christmasTreePageStyle.components_wrapper}>
+                <p className={christmasTreePageStyle.title}>ИГРУШКИ</p>
+                <div className={christmasTreePageStyle.toy_wrapper}>
+                    {data.map((obj, index) => {
+                        return (
+                            <div key={index} style={{ backgroundImage: `url(${obj.num})` }} className={christmasTreePageStyle.toy_block} onClick={() => { }}>
+                                <div className={christmasTreePageStyle.round}>
+                                    <p>{obj.count}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
 
-        </div> 
     )
 }
